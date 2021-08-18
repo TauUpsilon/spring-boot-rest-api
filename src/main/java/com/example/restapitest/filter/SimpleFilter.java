6222cfc50a476859fddfe31389276461430fe649
Jacob <jacoblindev@gmail.com>
2021-08-18 11:50:03 +0800

    Clean up unused code

--- src/main/java/com/example/restapitest/filter/SimpleFilter.java
+++ src/main/java/com/example/restapitest/filter/SimpleFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,21 +14,9 @@ import org.springframework.stereotype.Component;
 public class SimpleFilter implements Filter {
 
     @Override
-    public void destroy() {
-        // TODO Auto-generated method stub
-        Filter.super.destroy();
-    }
-
-    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
             throws IOException, ServletException {
         filterChain.doFilter(req, res);
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // TODO Auto-generated method stub
-        Filter.super.init(filterConfig);
     }
 
 }
